270852a73c5ea9c6a5bfecda355a784c5011037e
jcanosan <jmcs990@gmail.com>
2025-10-07 21:20:15 +0200

    Add single overlay visualization

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+
+  mainWindow.loadURL('http://proxy.iinact.com/overlay/ember_overlay/?HOST_PORT=ws://127.0.0.1:10501#/')
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
