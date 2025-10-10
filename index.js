ff022d8ed3485405aa0bceea5d8b7f0179af58ac
jcanosan <jmcs990@gmail.com>
2025-10-10 18:57:10 +0200

    Migrate to WebContentsView

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BaseWindow, WebContentsView } = require("electron");
+
+var nodeConsole = require("console");
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 400,
+  const mainWindow = new BaseWindow({
+    width: 500,
+    height: 360,
+    // frame: false,
+    backgroundColor: "#000000ff",
+    resizable: true,
   });
 
-  mainWindow.loadURL('http://proxy.iinact.com/overlay/ember_overlay/?HOST_PORT=ws://127.0.0.1:10501#/')
+  const iinactContent = new WebContentsView();
+  mainWindow.contentView.addChildView(iinactContent);
+  iinactContent.webContents.loadURL(
+    "http://proxy.iinact.com/overlay/kagerou/?HOST_PORT=ws://127.0.0.1:10501#/"
+  );
+  const bounds = mainWindow.getBounds();
+  iinactContent.setBounds({
+    x: 0,
+    y: 0,
+    width: bounds.width,
+    height: bounds.height,
+  });
+  iinactContent.setBackgroundColor("#000000ff");
+
+  mainWindow.on("resize", () => {
+    if (!mainWindow || !iinactContent) {
+      myConsole.log("No mainWindow or iinactOverlay");
+      return;
+    }
+    const bounds = mainWindow.getBounds();
+    iinactContent.setBounds({
+      x: 0,
+      y: 0,
+      width: bounds.width,
+      height: bounds.height,
+    });
+  });
 };
 
 app.whenReady().then(() => {
